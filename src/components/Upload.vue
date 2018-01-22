b4f7c93485c436a57df7c07991a8706e545e78d1
zhen-ke <cool420984@gmail.com>
2018-01-22 22:13:49 +0800

    初步完成七牛文件上传功能

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -1,64 +1,52 @@
 <template>
-  <label class="avatar-bg">
-    <!--默认显示的一张图片-->
-    <div class="avatar" v-if="!previewAvatar"></div>
-    <!--七牛回调的图片path-->
-    <img :src="previewAvatar" class="real-photo" v-if="previewAvatar">
-    <input ref="upload" id="upload" type="file" class="realfilebt" style="display: none;" />
-  </label>
+<el-upload
+  class="upload-demo"
+  drag
+  action="http://upload.qiniup.com"
+  :on-success='handleAvatarSuccess'
+  :on-error="handleError"
+  :before-upload="beforeAvatarUpload"
+  :data="postData"
+  multiple>
+   <img v-if="imageUrl" :src="imageUrl" class="avatar">
+  <i class="el-icon-upload"></i>
+  <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+  <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过2M</div>
+</el-upload>
 </template>
 
 <script>
   export default {
-    name: 'test',
     data() {
       return {
-        files: [], // 文件
-        uploadToken: '', // 上传文件 token
-        previewAvatar: '', // 页面展示的avatar
-        progress: 0, // 进度条
-      };
+        postData: {
+          token:'3sBXpWawR0hxP6Xxot3S8VaqiB43HjDP7QF3Ua_e:aM9dZ6ZGTK43h6OzCKAttEl4dBg=:eyJzY29wZSI6InhtaXQiLCJkZWFkbGluZSI6MTUyMDIyMjUyMn0='
+        },
+        imageUrl: ''
+      }
     },
-    //node版本要是8+，以上才能使用async await的方法
-    async mounted() {
-      let self = this;
-      //页面加载 拉去token
-      let tokenRes = await axios({
-        url: '/api/sz/upToken',
-      });
-      this.uploadToken = tokenRes.data.token;
-      this.$refs.upload.addEventListener('change', function () {
-        //console.log(this.files)
-        var data = new FormData();
-        data.append('token', self.uploadToken);
-        data.append('file', this.files[0]);
-        axiosInstance({
-            method: 'POST',
-            url: 'http://up.qiniu.com',
-            data: data,
-            onUploadProgress: function (progressEvent) {
-              var percentCompleted = Math.round(progressEvent.loaded * 100 / progressEvent.total);
-              //console.log(percentCompleted)
-              //对应上传进度条
-              self.progress = percentCompleted;
-            },
-          })
-          .then(function (res) {
-            //console.log('res',res)
-            let {
-              base_url,
-              path
-            } = res.data;
-            //页面所用字段
-            self.previewAvatar = `${base_url}${path}?imageView2/1/w/154/h/154`;
-            //传给后台不完整
-            self.formData.avatar = `${path}`;
-
-          })
-          .catch(function (err) {
-            console.log('err', err);
-          });
-      });
+    methods: {
+      handleAvatarSuccess(res, file) { //上传成功后在图片框显示图片
+        this.imageUrl = 'http://xmit.qiniudn.com/' + res.key
+        console.log(res)
+      },
+      handleError(res) { //显示错误
+        console.log(res)
+      },
+      beforeAvatarUpload(file) { //在图片提交前进行验证
+      console.log(file)
+        let isJPG = file.type === "image/jpeg"
+        let isPNG = file.type === "image/png"
+        let isLt2M = file.size / 1024 / 1024 < 2
+        if (!isJPG && !isPNG) {
+          this.$message.error('上传头像图片只能是 JPG/PNG 格式!')
+          return false
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!')
+          return false
+        }
+      }
     }
   }
 
