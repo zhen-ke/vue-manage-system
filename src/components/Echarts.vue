9c41c21798e4083d703bbd8ddeab119bdf5c66da
zhen-ke <cool420984@gmail.com>
2018-01-29 17:46:38 +0800

    添加Echarts第三方库

---
+++ src/components/Echarts.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="charts" style="width: 600px;height:400px;" ref="charts"></div>
+</template>
+
+<script>
+  import Echarts from "echarts";
+  export default {
+    props: {
+      options: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        chart: null
+      }
+    },
+    methods: {
+      init() {
+        this.chart = Echarts.init(this.$refs.charts);
+        this.chart.setOption(this.options)
+      }
+    },
+    mounted() {
+      this.init()
+    }
+  }
+
+</script>
+
+<style>
+
+
+</style>
--- src/page/settings/adminSettings.vue
+++ src/page/settings/adminSettings.vue
@@ -5,23 +5,73 @@
         <Upload></Upload>
       </el-form-item>
     </el-form>
+    <Echarts :options="options1"></Echarts>
+    <Echarts :options="options2"></Echarts>
   </div>
 </template>
 
 <script>
   import Upload from '@/components/Upload'
+  import Echarts from '@/components/echarts'
 
   export default {
     data() {
       return {
-        admin: ''
+        admin: '',
+        options1: {
+          title: {
+            text: 'ECharts 测试'
+          },
+          tooltip: {},
+          legend: {
+            data: ['销量']
+          },
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+          },
+          yAxis: {},
+          series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }]
+        },
+        options2: {
+          series: [{
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            data: [{
+                value: 235,
+                name: '视频广告'
+              },
+              {
+                value: 274,
+                name: '联盟广告'
+              },
+              {
+                value: 310,
+                name: '邮件营销'
+              },
+              {
+                value: 335,
+                name: '直接访问'
+              },
+              {
+                value: 400,
+                name: '搜索引擎'
+              }
+            ]
+          }]
+        }
       }
     },
     methods: {
 
     },
     components: {
-      Upload
+      Upload,
+      Echarts
     }
   }
 
